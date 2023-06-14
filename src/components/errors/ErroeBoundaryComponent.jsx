f3f865b50cd16c2fc3cf91f46d22a4332709dccb
Priyanka <priyanka@Apples-MacBook-Pro.local>
2023-06-14 17:32:57 +0530

    first commit

--- src/components/errors/ErroeBoundaryComponent.jsx
+++ src/components/errors/ErroeBoundaryComponent.jsx
@@ -3,9 +3,12 @@
 import { useEffect } from "react";
 import { useErrorBoundary } from "react-error-boundary";
 import ReactGA from "react-ga";
+import useGAEventTracker from "../../hooks/useGAEventTracker";
 
 export function ProdunctFetchingError({ error }) {
   const { resetBoundary } = useErrorBoundary();
+  const GAEventsTracker = useGAEventTracker("Product items");
+
 
   useEffect(()=>{
     ReactGA.exception({
@@ -18,7 +21,7 @@ export function ProdunctFetchingError({ error }) {
     <div className=" h-screen flex flex-col justify-center items-center bg-slate-400">
     <div className=" flex flex-col justify-center items-center bg-slate-600">
       <p>Something went wrong:</p>
-      <pre className=' m-auto text-red-600 text-center text-2xl'>{error.message}</pre>
+      <pre className=' m-auto text-red-600 text-center text-2xl' onClick={(e) =>GAEventsTracker("error found", `${error.message}`)}>{error.message}</pre>
       <button onClick={resetBoundary}>Try again</button>
     </div>
 
